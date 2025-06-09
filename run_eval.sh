c425fc6ea0fa6a0dafef20411fa2efeb18476130
jlpang <jlpang863@gmail.com>
2025-06-09 01:51:16 -0700

    icml update

--- run_eval.sh
+++ run_eval.sh
@@ -1,27 +1,62 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-NUM_GPUs=4
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+NUM_GPUs=8
 
-
-start_time=$(date +%s)
 
 MODEL=hf #hf
 
+### model path
+# model_path=/home/jlpang/LLM_token_selection/output/models/meta-llama/Llama-3.2-3B
 
-base_model=meta-llama/Llama-3.2-3B
+
+#### num_fewshot, batch_size, max_examples(less 1 means proportion)
+declare -A TASK_PARAMS=(
+    ["mmlu"]="5 16 0.99"
+    ["bbh"]="3 64 40"
+    ["gsm8k"]="8 48 200"
+    ["truthfulqa"]="0 128 0.99"
+    ["arc_challenge"]="0 32 0.99"
+    ["piqa"]="0 32 0.99"
+    ["hellaswag"]="0 128 0.99"
+    ["openbookqa"]="0 32 0.99"
+    ["sciq"]="0 32 0.99"
+    ["arc_easy"]="0 32 0.99"
+    ["logiqa"]="0 32 0.99"
+    ["boolq"]="0 32 0.99"
+    ["winogrande"]="0 32 0.99"
+)
+
+####### llama-3.1-8b #####
+declare -A TASK_PARAMS=(
+    ["mmlu"]="5 8 0.99"
+    ["bbh"]="3 32 40"
+    ["gsm8k"]="8 48 200"
+    ["truthfulqa"]="0 128 0.99"
+    ["arc_challenge"]="0 32 0.99"
+    ["piqa"]="0 32 0.99"
+    ["hellaswag"]="0 128 0.99"
+    ["openbookqa"]="0 32 0.99"
+    ["sciq"]="0 32 0.99"
+    ["arc_easy"]="0 32 0.99"
+    ["logiqa"]="0 32 0.99"
+    ["boolq"]="0 32 0.99"
+    ["winogrande"]="0 32 0.99"
+)
+
+
+# base_model=meta-llama/Llama-3.2-3B
 # base_model="meta-llama/Llama-3.1-8B-Instruct"
-# base_model="meta-llama/Llama-3.1-8B"
+base_model="meta-llama/Llama-3.1-8B"
 # base_model="mistralai/Mistral-7B-v0.3"
 
-# Train_DATASET_LIST=("base")
-# Train_DATASET_LIST=("filtered-cured-50k-rho-baseline-llama3b-global-low-ppl")
-# Train_DATASET_LIST=("filtered-cured-50k-rho-baseline-llama3b-global-low-ppl-0.86")
-Train_DATASET_LIST=("filtered-cured-50k-rho-baseline-llama3b-global-low-ppl" "filtered-cured-50k-rho-baseline-llama3b-global-high-ppl")
-data_prop_list=(0.6)
-# TASK_LISTS=('mmlu' "truthfulqa" "hellaswag" "arc_challenge" "boolq" 'logiqa')
 
-TASK_LISTS=('mbpp' "gsm8k")
+Train_DATASET_LIST=(
+    "filtered-cured-50k-iter-split-global_data_prop_0.6_mistral-non-filtered-fixed-base-model_4"
+    ) 
+
+data_prop_list=(0.6) # 0.3
 
 
+TASK_LISTS=('mmlu' "truthfulqa" "hellaswag" "arc_challenge" "boolq" 'logiqa')
 
 for train_dataset_name in "${Train_DATASET_LIST[@]}" 
 do
@@ -40,12 +75,11 @@ do
     ##############################################################
     model_path="/mnt/data1/jinlong/token_selection_output/models/${base_model}"
     model_tags=("${train_dataset_name}")
+
     ##############################################################
 
 
     if [[ "$train_dataset_name" == *"llama3b"* ]]; then
-    #### num_fewshot, batch_size, max_examples(less 1 means proportion)
-
         declare -A TASK_PARAMS=(
             ["mmlu"]="5 16 0.99"
             ["bbh"]="3 64 40"
@@ -64,7 +98,6 @@ do
             ["squad_completion"]="0 64 0.99"
             ["triviaqa"]="0 64 0.99"
             ["humaneval"]="0 64 0.99"
-            ["mbpp"]="8 48 0.99"
 
         )
     else
@@ -86,7 +119,6 @@ do
             ["squad_completion"]="0 64 0.99"
             ["triviaqa"]="0 64 0.99"
             ["humaneval"]="0 64 0.99"
-            ["mbpp"]="8 48 0.99"
 
         )
     fi
@@ -131,8 +163,6 @@ do
                 ["triviaqa"]="pretrained=${pretrained_model},dtype=bfloat16"
                 ["squad_completion"]="pretrained=${pretrained_model},dtype=bfloat16"
                 ["humaneval"]="pretrained=${pretrained_model},dtype=bfloat16"
-                ["mbpp"]="pretrained=${pretrained_model},dtype=bfloat16"
-
             )
 
 
@@ -167,26 +197,19 @@ do
             ###########################################
             #### tydiqa eval ####
             ###########################################
-            # CUDA_VISIBLE_DEVICES=0 python -m eval.tydiqa.run_eval \
-            #     --data_dir raw_data/eval/tydiqa/ \
-            #     --n_shot 1 \
-            #     --max_num_examples_per_lang 100 \
-            #     --max_context_length 512 \
-            #     --save_dir $OUTPUT_PATH \
-            #     --model_name_or_path $pretrained_model \
-            #     --tokenizer_name_or_path $pretrained_model \
-            #     --eval_batch_size 15 \
-            #     --use_vllm
+            CUDA_VISIBLE_DEVICES=0 python -m eval.tydiqa.run_eval \
+                --data_dir raw_data/eval/tydiqa/ \
+                --n_shot 1 \
+                --max_num_examples_per_lang 100 \
+                --max_context_length 512 \
+                --save_dir $OUTPUT_PATH \
+                --model_name_or_path $pretrained_model \
+                --tokenizer_name_or_path $pretrained_model \
+                --eval_batch_size 5
 
         done
     done
 done 
 
-echo "all experiments finished!!"
 
-end_time=$(date +%s)
-elapsed_time=$((end_time - start_time))
-minutes=$((elapsed_time / 60))
-
-echo "Elapsed time: $elapsed_time seconds"
 
